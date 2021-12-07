84a18010e6c21d3318406d1a7a5e5d8622b3f26f
BananaSoftwaree <93658686+BananaSoftwaree@users.noreply.github.com>
2021-12-07 10:22:48 +0700

    Upload Windows 95 Key Generator
    The setup file and source file of Windows 95 Key Generator by Banana Software. Made using Python.

---
+++ src/Windows 95 Keygen/Windows 95 Key Generator.py
@@ -0,0 +1,64 @@
+from tkinter import *
+import random
+from abc import ABC
+import datetime
+
+top = Tk()
+top.geometry("320x150")
+top.iconbitmap('Windows 95.ico')
+top.title('Windows 95 Key Generator')
+top.resizable(width=False, height=False)
+
+class KeyGenerator(ABC):
+    @staticmethod
+    def num_digits(num):
+        ct = 0
+        while num > 0:
+            ct += 1
+            num //= 10
+        return ct
+
+    @staticmethod
+    def sum_of_digits(num):
+        sm = 0
+        while num > 0:
+            rem = num % 10
+            sm += rem
+            num //= 10
+        return sm
+
+def buttonClick():
+    doy = random.randint(1, 367)
+    length = KeyGenerator.num_digits(doy)
+    doystring = ""
+    for i in range(0, 3 - length):
+        doystring += "0"
+    doystring += str(doy)
+    ystring = random.choice(["95", "96", "97", "98", "99", "00", "01", "02", "03"])
+    x2 = 1
+    x2str = "0"
+    while KeyGenerator.sum_of_digits(x2) % 7 != 0:
+        x2 = random.randint(0, 1000000)
+        while x2 % 10 == 0 or x2 % 10 == 8 or x2 % 10 == 9:
+            x2 = random.randint(0, 1000000)
+    length = KeyGenerator.num_digits(x2)
+    for i in range(0, 6 - length):
+        x2str += "0"
+    x2str += str(x2)
+    x3 = random.randint(0, 100000)
+    x3str = ""
+    for i in range(0, 5 - length):
+        x3str += "0"
+    x3str += str(x3)
+
+    key = f'{doystring}{ystring}-OEM-{x2str}-{x3str}'
+    keylabel = Label(top, text=f'Key: {key}').place(x = 70, y = 57.5)
+
+title = Label(top, text='Windows 95 Key Generator').place(x = 85, y = 10)
+creator = Label(top, text='Creator: Banana Software').place(x = 87.5, y = 30)
+keylabel = Label(top, text='Press \'Generate\' to generate key!').place(x = 70, y = 57.5)
+genbutton = Button(top, text = " Generate ", command=buttonClick).place(x = 85, y = 87.5)
+exit_button = Button(top, text="     Exit     ", command=top.destroy).place(x = 170, y = 87.5)
+now = datetime.datetime.now()
+keylabel = Label(top, text=f'Version 1.0 - Copyright © {now.year} by Banana Software™').place(x = 20, y = 125)
+top.mainloop()
