7497f134e7e0cf67f6914174ad97a1bb7784ce58
BananaSoftwaree <93658686+BananaSoftwaree@users.noreply.github.com>
2021-12-09 13:03:29 +0700

    Windows 95 Keygen Version 1.1 + Important Update
    Windows 95 Keygen Version 1.1
    - Update modern button support (base on your system)
    
    Important update:
    - No more compressed file till the launcher for all tool is finish.

--- src/Windows 95 Keygen/Windows 95 Key Generator.py
+++ src/Windows 95 Keygen/Windows 95 Key Generator.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from tkinter import ttk
 import random
 from abc import ABC
 import datetime
@@ -52,13 +53,13 @@ def buttonClick():
     x3str += str(x3)
 
     key = f'{doystring}{ystring}-OEM-{x2str}-{x3str}'
-    keylabel = Label(top, text=f'Key: {key}').place(x = 70, y = 57.5)
+    keylabel = ttk.Label(top, text=f'Key: {key}').place(x = 70, y = 57.5)
 
-title = Label(top, text='Windows 95 Key Generator').place(x = 85, y = 10)
-creator = Label(top, text='Creator: Banana Software').place(x = 87.5, y = 30)
-keylabel = Label(top, text='Press \'Generate\' to generate key!').place(x = 70, y = 57.5)
-genbutton = Button(top, text = " Generate ", command=buttonClick).place(x = 85, y = 87.5)
-exit_button = Button(top, text="     Exit     ", command=top.destroy).place(x = 170, y = 87.5)
+title = ttk.Label(top, text='Windows 95 Key Generator').place(x = 85, y = 10)
+creator = ttk.Label(top, text='Creator: Banana Software').place(x = 87.5, y = 30)
+keylabel = ttk.Label(top, text='Press \'Generate\' to generate key!').place(x = 70, y = 57.5)
+genbutton = ttk.Button(top, text = " Generate ", command=buttonClick).place(x = 70, y = 87.5)
+exit_button = ttk.Button(top, text="     Exit     ", command=top.destroy).place(x = 165, y = 87.5)
 now = datetime.datetime.now()
-keylabel = Label(top, text=f'Version 1.0 - Copyright © {now.year} by Banana Software™').place(x = 20, y = 125)
+keylabel = ttk.Label(top, text=f'Version 1.1 - Copyright © {now.year} by Banana Software™').place(x = 20, y = 125)
 top.mainloop()
